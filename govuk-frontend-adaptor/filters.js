42993302dcf31dce75b47145f7bb0d4bceb70972
Natalie Carey <natalie@nowprototype.it>
2025-05-31 19:33:34 +0100

    Adding GOV.UK Rebrand setting to the adaptor.

--- govuk-frontend-adaptor/filters.js
+++ govuk-frontend-adaptor/filters.js
@@ -1,5 +1,23 @@
+const { addFunction } = require('nowprototypeit').views
+const { getPluginSpecificConfig } = require('nowprototypeit').config
 
-addFilter(
+const GOVUKRebrandStyles = !!getPluginSpecificConfig('@nowprototypeit/govuk-frontend-adaptor').GOVUKRebrandStyles
+
+
+/**
+ * This is intended to match the behaviour of the govuk-frontend functionality, which needs to also be compatible
+ * with the GOV.UK Prototype Kit.  This is used to set a nunjucks variable which can easily be set from the
+ * plugin-specific config (see the "serviceName" config in now-prototype-it-config.js as a clear example of this).
+ * In this case we have to match their approach.
+ *
+ * During the process of building this, it became clear that plugin authors might benefit from the ability to read
+ * the config in an officially supported way, so we've added require('nowprototypeit').config.getConfig()
+ */
+
+addFunction('govukRebrand', () => GOVUKRebrandStyles)
+
+
+addFunction(
   'formatItems',
   /**
    * Returns an array of objects for use in a macro that requires a list of items
